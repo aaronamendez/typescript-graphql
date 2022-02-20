404e04498eb895fd54e36527ba52a322f902b9b3
Ant Man <34754995+aaronamendez@users.noreply.github.com>
2022-02-20 00:59:08 -0500

    GraphQL is live.

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,40 @@
 import express, { Application, Request, Response } from "express";
+import { graphqlHTTP } from "express-graphql";
+import { buildSchema } from "graphql";
 const server: Application = express();
 
-server.get("/", (req: Request, res: Response) => {
-  res.json("Hello, World... From Typescript 👍");
-});
+const schema = buildSchema(`
+  type Query {
+    hello: String,
+    goodbye: String
+  }
+  
+`);
+
+const root = {
+  hello: () => {
+    return "Hello, World... From GraphQL 👍";
+  },
+  goodbye: () => {
+    return "Goodbye, World!.... Also from GraphQL 👍";
+  },
+};
+
+server.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+  })
+);
 
 const PORT: number = Number(process.env.PORT) || 4000;
 
 server.listen(PORT, () => {
   console.log(`<=== || Server is running on PORT: ${PORT} || ===>`);
 });
+
+// server.get("/", (req: Request, res: Response) => {
+//   res.json("Hello, World... From Typescript 👍");
+// });
